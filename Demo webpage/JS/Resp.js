3d360d558d613e0b49e1c78a5b74d00a01232e47
MaheshP1997 <145980510+MaheshP1997@users.noreply.github.com>
2023-09-25 15:41:00 +0530

    Add files via upload

---
+++ Demo webpage/JS/Resp.js
@@ -0,0 +1,10 @@
+hamburger = document.querySelector(".hamburger")
+navbar = document.querySelector(".navbar")
+Rightsidenav = document.querySelector(".Rightsidenav")
+navlist = document.querySelector(".navlist")
+
+hamburger.addEventListener("click",()=>{
+navbar.ClassList.toggle('.h-nav');
+
+}
+)
